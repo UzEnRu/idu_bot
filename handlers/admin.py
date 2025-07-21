b95da03446c7a618ce8317ddfc9989b44ce32f63
UzEnRu <seniorleo89@gmail.com>
2025-07-21 13:28:23 +0500

    admin panel changed to interactive

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,7 +1,15 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
 async def contact_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_html(
-        "📬 Admin bilan bog‘lanish: <a href='https://t.me/thelxn'>@thelxn</a>"
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("📬 Xabar yuborish", url="https://t.me/thelxn")],
+        [InlineKeyboardButton("🔙 Orqaga", callback_data="go_back")]
+    ])
+
+    text = (
+        "👤 <b>Admin bilan bog‘lanish</b>\n\n"
+        "Ismingizni, muammo yoki taklifingizni to‘liq yozib yuboring.\n\n"
+        "Admin siz bilan Telegram orqali bog‘lanadi."
     )
+    await update.message.reply_html(text, reply_markup=keyboard)
